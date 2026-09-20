50d8281a8e2b212a497815015babd31d62e75046
Moilab <moilab.web@gmail.com>
2026-09-20 15:11:14 +0200

    feat(): custom hooks connected btw them

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
 // import { TrafficLight } from "./01-useState/TrafficLight";
 // import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect";
+// import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
+import { PokemonPage } from "./03-examples/PokemonPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
-    <TrafficLightWithHook />
+    {/* <TrafficLightWithHook /> */}
+    <PokemonPage />
   </StrictMode>,
 );
