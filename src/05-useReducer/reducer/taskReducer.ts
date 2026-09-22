e32be5e2e5e48ca9dee81cd0716218a0f25fcd20
Moilab <moilab.web@gmail.com>
2026-09-22 15:41:55 +0200

    feat(): tasReducer funciton

--- src/05-useReducer/reducer/taskReducer.ts
+++ src/05-useReducer/reducer/taskReducer.ts
@@ -12,10 +12,76 @@ export type TaskAction =
   | { type: "TOGGLE_TODO"; payload: number }
   | { type: "DELETE_TODO"; payload: number };
 
+const getTodosStats = (todos: Todo[]) => {
+  const todosLength = todos.length;
+  const completed = todos.filter((todo) => todo.completed).length;
+  const pending = todos.filter((todo) => !todo.completed).length;
+
+  return {
+    length: todosLength,
+    completed,
+    pending,
+  };
+};
+
 export const taskReducer = (
   state: TaskState,
   action: TaskAction,
 ): TaskState => {
-    
-  return state;
+  switch (action.type) {
+    case "ADD_TODO": {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: action.payload.trim(),
+        completed: false,
+      };
+
+      const newTodos = [...state.todos, newTodo];
+      const { length, completed, pending } = getTodosStats(newTodos);
+
+      return {
+        ...state,
+        todos: newTodos,
+        length,
+        completed,
+        pending,
+      };
+    }
+    case "TOGGLE_TODO": {
+      const newTodos = state.todos.map((todo) => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
+
+      const { length, completed, pending } = getTodosStats(newTodos);
+
+      return {
+        ...state,
+        todos: newTodos,
+        length,
+        completed,
+        pending,
+      };
+    }
+    case "DELETE_TODO": {
+      const newTodos = state.todos.filter((todo) => todo.id !== action.payload);
+      const { length, completed, pending } = getTodosStats(newTodos);
+
+      return {
+        ...state,
+        todos: newTodos,
+        length,
+        completed,
+        pending,
+      };
+    }
+
+    default:
+      return state;
+  }
 };
